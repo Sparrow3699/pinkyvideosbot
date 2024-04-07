74dd2e2fa1dcdfaf5daf77b8b05603c918ede6a4
Sparrow <149317656+Sparrow3699@users.noreply.github.com>
2024-04-07 21:25:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "1392184089"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Pinkybitlu:pinkybitlu@pinkybitlu.tw3benc.mongodb.net/?retryWrites=true&w=majority&appName=PinkyBitlu")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://pinkybitlu:pinky7268@cluster0.dizew5m.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "pinkybitlu")
 
 #force sub channel id, if you want enable force sub
